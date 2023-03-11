d4ea3428c99067eff455f781ee1898a40035bb48
abdur rahman shabab khan <110523897+abdurrahman0123@users.noreply.github.com>
2023-03-11 13:34:53 +0530

    Update and rename te.py to test.py

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import socket
 
-
-# The port number that Unity is listening on
-
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverAddressPort = ("127.0.0.1", 5005)
 data = "dsbhasdkgb"
-while True:
+
+while True: # for continues
     
     sock.sendto(str.encode(str(data)), serverAddressPort)
